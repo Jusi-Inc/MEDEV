2a981f00a953d35f16ed462a72767522571e3bb1
Alexandre Mestre <alexandre>
2026-01-12 16:34:08 +0100

    développement de la fonction de lecture des fichiers

--- PartielMedev/src/main/java/infosi/partielmedev/Dictionnary.java
+++ PartielMedev/src/main/java/infosi/partielmedev/Dictionnary.java
@@ -7,10 +7,10 @@ package infosi.partielmedev;
 import java.util.ArrayList;
 import java.util.Random;
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.List;
 
 /**
- *
+ * Classe permettant de choisir un mot au hasard
  * @author Catherine
  */
 public class Dictionnary {
@@ -25,17 +25,17 @@ public class Dictionnary {
 	public Dictionnary(String file) {
 		rand = new Random();
 		filename = file;
-		wordList = new ArrayList<String>();
+		wordList = new ArrayList<>();
 	}
 
 	/**
 	 * Contructeur pour avoir un mot depuis une liste
 	 * @param list La liste à copier
 	 */
-	public Dictionnary(ArrayList list) {
+	public Dictionnary(List<String> list) {
 		rand = new Random();
 		filename = "";
-		wordList = new ArrayList<String>(list);
+		wordList = new ArrayList<>(list);
 	}
 
 	/**
@@ -45,72 +45,131 @@ public class Dictionnary {
 	public Dictionnary(Dictionnary dico) {
 		rand = new Random();
 		filename = dico.getFilename();
-		wordList = new ArrayList<String>(dico.getWordList());
+		wordList = new ArrayList<>(dico.getWordList());
 	}
 
 	/**
-	 * Choisit un mot au hasard. Lit le fichier sila liste est vide
+	 * Choisit un mot au hasard
 	 * @return Un mot
 	 */
 	public String pickWord() {
-		String res = "";
-
-		// Essaie de lire le fichier si la liste est vide		
-		if (wordList.size() == 0) {
-			readFile();
+		// Affiche une erreur si la liste est vide
+		if (wordList.isEmpty()) {
+			throw new ArrayIndexOutOfBoundsException();
 		}
 
 		// Trouve un index
 		int index = rand.nextInt(wordList.size());
 
-		// Copie le mot
-		res = wordList.get(index);
-
-		return res;
+		return wordList.get(index);
 	}
 
+	/**
+	 * Choix d'un mot en particulier
+	 * @param i Un index
+	 * @return
+	 */
 	public String chooseWord(int i) {
 		// Erreur si l'index est trop grand
 		if (i >= wordList.size()) {
-			// TODO
+			throw new ArrayIndexOutOfBoundsException();
 		}
-
 		return wordList.get(i);
+	}
+
+	/**
+	 * Retourne le nombre de mot dans le dictionnaire
+	 * @return La taille de la liste
+	 */
+	public int listSize() {
+		return wordList.size();
 	}
 
 
 	/**
 	 * Lecture du fichier
 	 * @return Vrai si la lecture à été un succès
+	 * @throws java.lang.Exception
 	 */
-	public boolean readFile() {
+	public boolean readFile() throws Exception { // NOSONAR
 		boolean res = false;
-
-		// TODO
+		
+		BufferedReader file = null;
+		
+		try {
+			String line;
+			file = new BufferedReader(new FileReader(filename)); // NOSONAR
+			
+			wordList.clear();
+			
+			// Constitution de la liste
+			line = file.readLine();
+			while (line != null) {
+				String word = traitementDuMot(line);
+				if (!word.isEmpty()) {
+					wordList.add(word);
+				}
+			
+			}
+			file.close();
+			res = true;
+		} catch(IOException e) {
+			throw new Exception("Erreur lors de la lecture de la sauvegarde"); // NOSONAR
+		}
 
 		return res;
 	}
 
+	private String traitementDuMot(String mot) {
+		mot = mot.trim();
+		mot = mot.toUpperCase();
+		return mot;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
 	public String getFilename() {
 		return filename;
 	}
 
+	/**
+	 *
+	 * @param filename
+	 */
 	public void setFilename(String filename) {
 		this.filename = filename;
 	}
 
-	public ArrayList<String> getWordList() {
+	/**
+	 *
+	 * @return
+	 */
+	public List<String> getWordList() {
 		return wordList;
 	}
 
-	public void setWordList(ArrayList<String> wordList) {
-		this.wordList = wordList;
+	/**
+	 *
+	 * @param wordList
+	 */
+	public void setWordList(List<String> wordList) {
+		this.wordList = new ArrayList<>(wordList);
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public Random getRand() {
 		return rand;
 	}
 
+	/**
+	 *
+	 * @param rand
+	 */
 	public void setRand(Random rand) {
 		this.rand = rand;
 	}
