f4a79afbd6cc85464f2da2d048ad44992aeb68c1
alex_el.arte <77207986+alexElArte@users.noreply.github.com>
2026-01-12 08:37:39 +0100

    Hello World

---
+++ PartielMedev/src/main/java/infosi/partielmedev/PartielMedev.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package infosi.partielmedev;
+
+/**
+ *
+ * @author Catherine
+ */
+public class PartielMedev {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
